56316c4d6318f732c9fb6e985ca651aba457e0f1
Eric Zhang <ericbkzhang@gmail.com>
2026-05-18 23:24:06 -0400

    把`binary_search_tree.rs`的用户界面翻译成中文

--- binary-search-tree/src/main.rs
+++ binary-search-tree/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     // Check for valid number of arguments.
     let args: Vec<String> = std::env::args().collect();
     if std::env::args().len() < 2 {
-        panic!("Usage: {} <string, int/integer>", args[0]);
+        panic!("用法：{} <string、int/integer>", args[0]);
     }
 
     let mut tree;
@@ -21,18 +21,18 @@ fn main() {
             tree = BinarySearchTreeType::Integer(BinarySearchTree::new());
         }
         &_ => {
-            panic!("\"{}\" is not a valid type.", args[1]);
+            panic!("\"{}\"不是有效的类型。", args[1]);
         }
     }
 
     loop {
-        print!("Enter element to insert: ");
+        print!("输入想插入的元素：");
         let _ = std::io::stdout().flush();
 
         let mut input = String::new();
         std::io::stdin()
             .read_line(&mut input)
-            .expect("Invalid input type.");
+            .expect("输入值无效。");
         input = input.trim_end().to_string();
         println!();
 
@@ -40,14 +40,14 @@ fn main() {
             BinarySearchTreeType::Integer(ref mut integer_tree) => integer_tree.add(
                 input
                     .parse::<i32>()
-                    .expect(format!("\"{input}\" is not an integer.\n").as_str()),
+                    .expect(format!("\"{input}\"不是整数。\n").as_str()),
             ),
             BinarySearchTreeType::String(ref mut string_tree) => {
                 string_tree.add(input);
             }
         }
 
-        println!("Tree size: {}", tree.size());
-        println!("Tree:\n{}", tree.to_string());
+        println!("树大小：{}", tree.size());
+        println!("树：\n{}", tree.to_string());
     }
 }
