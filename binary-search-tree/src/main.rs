ea7b689c01ea2d418338fd4614ea2ef22a09a241
Eric Zhang <ericbkzhang@gmail.com>
2026-05-19 22:01:37 -0400

    加生产随机值

--- binary-search-tree/src/main.rs
+++ binary-search-tree/src/main.rs
@@ -8,24 +8,51 @@ use binary_search_tree::BinarySearchTreeType;
 fn main() {
     // Check for valid number of arguments.
     let args: Vec<String> = std::env::args().collect();
-    if std::env::args().len() < 2 {
-        panic!("用法：{} <string、int/integer>", args[0]);
+    if args.len() < 2 {
+        panic!("用法：{} <string、int/integer> <随机值数>", args[0]);
     }
+
+    let num_random = if args.len() >= 3 {
+        args[2]
+            .parse::<i32>()
+            .expect(format!("\"{}\"不是整数。\n", args[2]).as_str())
+    } else {
+        0
+    };
 
     let mut tree;
     match args[1].as_str() {
-        "string" => {
-            tree = BinarySearchTreeType::String(BinarySearchTree::new());
-        }
         "int" | "integer" => {
             tree = BinarySearchTreeType::Integer(BinarySearchTree::new());
         }
+
+        "string" => {
+            tree = BinarySearchTreeType::String(BinarySearchTree::new());
+        }
+
         &_ => {
             panic!("\"{}\"不是有效的类型。", args[1]);
         }
     }
 
+    match tree {
+        BinarySearchTreeType::Integer(ref mut integer_tree) => {
+            for _i in 0..num_random {
+                integer_tree.add(rand::random::<i32>());
+            }
+        }
+
+        BinarySearchTreeType::String(ref mut string_tree) => {
+            for _i in 0..num_random {
+                string_tree.add(rand::random::<char>().to_string());
+            }
+        }
+    }
+
     loop {
+        println!("树大小：{}", tree.size());
+        println!("树：\n{}", tree.to_string());
+
         print!("输入想插入的元素：");
         let _ = std::io::stdout().flush();
 
@@ -42,12 +69,7 @@ fn main() {
                     .parse::<i32>()
                     .expect(format!("\"{input}\"不是整数。\n").as_str()),
             ),
-            BinarySearchTreeType::String(ref mut string_tree) => {
-                string_tree.add(input);
-            }
+            BinarySearchTreeType::String(ref mut string_tree) => string_tree.add(input),
         }
-
-        println!("树大小：{}", tree.size());
-        println!("树：\n{}", tree.to_string());
     }
 }
